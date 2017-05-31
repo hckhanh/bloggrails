2161c1a3370b2cd6b56ace4feec3fb7cda75ddf7
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-05-31 10:02:30 +0700

    Add expired times for authentication cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate, except: :destroy
 
   def new
+    puts Rails.configuration.expired_time
     @session = Session.find_by_session_token cookies.encrypted[:session_token]
     redirect_to redirect_path if @session
   end
@@ -10,15 +11,18 @@ class SessionsController < ApplicationController
   def create
     login = params[:login]
     password = params[:password]
-    user = get_email_or_username login
+    user = user_from_email_or_username login
 
     if user.try :authenticate, password
       session = user.sessions.create session_token: SecureRandom.urlsafe_base64(64)
-      cookies.encrypted[:session_token] = session.session_token
+      cookies.encrypted[:session_token] = {
+        value: session.session_token,
+        expires: Rails.configuration.expired_time.from_now
+      }
 
       redirect_to redirect_path
     else
-      flash[:error] = 'Invalid account credentials!'
+      flash.now[:error] = 'Invalid account credentials!'
       render :new
     end
   end
@@ -31,7 +35,7 @@ class SessionsController < ApplicationController
 
   private
 
-  def get_email_or_username(login)
+  def user_from_email_or_username(login)
     is_email(login) ? User.find_by_email(login) : User.find_by_username(login)
   end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,33 +2,33 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes                     = true
+  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load                        = true
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets            = true
+  config.read_encrypted_secrets = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled        = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor              = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile                    = false
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -49,10 +49,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level                         = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags                          = [:request_id]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "demo_rails511_#{Rails.env}"
-  config.action_mailer.perform_caching     = false
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -68,24 +68,27 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks                    = true
+  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation        = :notify
+  config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter                     = ::Logger::Formatter.new
+  config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Expired time for authentication token
+  config.expired_time = 2.hours
 end
