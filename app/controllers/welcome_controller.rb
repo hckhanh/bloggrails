73e5a608935bf4ffa9bae65fd950a9f464d6b2a8
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-06-04 09:36:40 +0700

    Authorize user in instruction page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate
+  before_action :authorized_user
 
   def instruction
     @content = Kramdown::Document.new(File.read("#{Rails.root}/README.md")).to_html.html_safe
